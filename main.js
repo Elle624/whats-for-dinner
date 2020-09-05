67b63d4142bf45082b79acc519ad3e3724001d89
Elle Li <yiran.li@hotmail.com>
2020-09-04 18:25:50 -0600

    Finish Iteration 3

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var userChoice = document.querySelectorAll('input');
 var text = document.querySelector('.text');
-var recipe = document.querySelector(".recipe")
-var pot = document.querySelector('#pot')
+var recipe = document.querySelector('.recipe');
+var pot = document.querySelector('#pot');
 var cookButton = document.querySelector('.cook');
+var clearButton = document.querySelector('.clear-button');
+var leftSideHtml = document.querySelector('.list');
 
+leftSideHtml.addEventListener('click', enableCookButton)
 cookButton.addEventListener('click', showRandomDish);
+clearButton.addEventListener('click', eraseRecipe);
 
+function disableCookButton() {
+    cookButton.disabled = true;
+}
+function enableCookButton() {
+    cookButton.disabled = false;
+}
 function createRandomNum(arr) {
     return Math.floor(Math.random()*arr.length);
 }
@@ -20,7 +30,7 @@ function createRandomDish() {
         } else if (userChoice[i].checked && i===3){
         recipe.innerText = 
         `${sides[createRandomNum(sides)]} with a side of ${mainDishes[createRandomNum(mainDishes)]} and ${desserts[createRandomNum(desserts)]} for dessert!`
-        } 
+        }
     }
 }
 function showRandomDish() {
@@ -28,3 +38,9 @@ function showRandomDish() {
     pot.classList.toggle('hidden');
     text.classList.toggle('hidden');
 } 
+function eraseRecipe() {
+    text.classList.add('hidden');
+    pot.classList.remove('hidden');
+}
+
+disableCookButton()
